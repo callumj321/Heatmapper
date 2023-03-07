9f0ceef655869f3c70296c411d1c570291febc61
callumj321 <callumj321@gmail.com>
2023-03-07 17:26:07 +0800

    Update

--- heatmapper.py
+++ heatmapper.py
@@ -2,6 +2,7 @@ import MDAnalysis as mda
 from MDAnalysis import analysis
 from MDAnalysis.analysis import contacts
 from MDAnalysis.analysis.contacts import contact_matrix
+import mdtraj as md
 import pandas as pd
 import numpy as np
 import multiprocessing
@@ -15,7 +16,8 @@ import time
 # from numba import jit, cuda
 # from timeit import default_timer as timer  
 
-quotes = ['"Lets-a go" - Chris Pratt','"I look very much forward to showing my financials, because they are huge." - Donald Trump','"I did not copy Gromacs" - Some rad guy, probably']
+quotes = ['"Lets-a go" - Chris Pratt','"I look very much forward to showing my financials, because they are huge." - Donald Trump','"I did not copy Gromacs" - Some rad guy, probably',
+'"ff at 15" - Abraham Lincoln']
 
 def help(self,func=''):
     if func == '':
@@ -51,30 +53,34 @@ def help(self,func=''):
 class traj_analysis:
     def __new__(cls, *args, **kwargs):
         return super().__new__(cls)
-    def __init__(self,pdb_filename,xtc_filename):
+    def __init__(self,pdb_filename,xtc_filename,analyte='protein'):
         self.pdb_filename = pdb_filename
         self.xtc_filename = xtc_filename
-        self.mda_universe = mda.Universe(pdb_filename, xtc_filename) # <--- Load in universe
+        print('Loading in mda universe...')
+        self.mda_universe = mda.Universe(pdb_filename,xtc_filename) # <--- Load in universe
+        print('Done.')
+        # self.mdtraj_universe = md.load(pdb_filename,xtc_filename)
         # self.mda_universe_coords = mda.Universe(pdb_filename)
-        self.protein = self.mda_universe.select_atoms('protein') # <--- Find the protein
+        self.analyte_loaded = self.mda_universe.select_atoms(analyte)
+        self.analyte_resids = np.unique(self.analyte_loaded.residues.resids)
         self.n_jobs = cpu_count() # <--- CPUs available for multithreading
         self.n_frames_tot = self.mda_universe.trajectory.trajectory.n_frames # <--- Find total number of frames in the trajectory
         rnd_qt = random.randint(0,int(len(quotes)-1))
-        print("- Run the heatmapper.help() function if you're lost")
-        print(str(quotes[rnd_qt]))
+        print(str(quotes[rnd_qt])+'\n')
+        print(str(len(np.unique(self.analyte_loaded.segids)))+' segids loaded.')
+        print(str(len(self.analyte_resids))+' resids per segid loaded.\n')
+        print("\n- Run the heatmapper.help() function if you're lost")
     def frame_to_time(self,frame_in):
         self.time = frame_in*self.mda_universe.trajectory.dt
         return self.time
     def time_to_frame(self,time_in):
         self.frame = time_in/self.mda_universe.trajectory.dt
         return self.frame
-    def pre_cont_pro(self,start,stop,skip,analyte):
+    def pre_cont_pro(self,start,stop,skip):
         frame_values = np.arange(self.mda_universe.trajectory.n_frames) # <--- Get all frames
         print('Frames: '+str(int(len(frame_values[start:stop:skip]))))
         print('Cores: '+str(int(self.n_jobs)))
-        print('Pases: '+str(int(len(frame_values[start:stop:skip])/self.n_jobs)))
-        print('Residues: '+str(len(self.mda_universe.select_atoms(analyte).residues.resids)))
-        return self.frame
+        print('Passes: '+str(int(len(frame_values[start:stop:skip])/self.n_jobs)))
     def cont_per_frame(self,frame_index,cont_dist,segid,analyte,probe_sel,carbon): # <--- The actual function which is executed on each CPU
         start_time = time.time()
         print('Reached frame '+str(frame_index)+'           ',end='\r')
@@ -109,7 +115,7 @@ class traj_analysis:
         print('Got segids. ',str(len(self.pro_segid)),' segids to analyse.')
         resids_in_pro = self.mda_universe.select_atoms(analyte).resids
         self.pro_resid = []
-        for resid in resids_in_pro:
+        for resid in self.analyte_resids:
             if resid not in self.pro_resid:
                 self.pro_resid.append(resid) # <--- Get a list of resids in the protein
         print('Got resids. ',str(len(self.pro_resid)),' resids to analyse.')
@@ -138,13 +144,16 @@ class traj_analysis:
             segid_iter+=1
         # self.gen_map(self.contact_dfs,analyte)
         return self.contact_dfs
-    def gen_map(self,df_data,analyte,res_op=0.6,res_rep='spacefill',BB_only=True):
-        segid_iter = 0
-        conts_per_resid = []
-        resid_list = self.mda_universe.select_atoms(analyte).residues.resids
+    def gen_map(self,df_data,analyte,res_op=1,res_rep='spacefill',res_rad=0.5,BB_only=True):
+        # Standardise df so that the resids are the index.
+        print('Standardising input')
+        if 'resids' in df_data.columns.values:
+            df_data = df_data.set_index('resids')
         print('Getting contact totals')
-        for resid in df_data.resids:
-            conts_per_resid.append(df_data.query('resids == @resid').values.sum())
+        conts_per_resid = []
+        for resid in self.analyte_resids:
+            conts_per_resid.append(df_data.query('index == @resid').values.sum())
+        # return conts_per_resid
         print('Got contact totals',end='\r')
         self.min_conts = min(conts_per_resid)
         self.max_conts = max(conts_per_resid)
@@ -156,54 +165,74 @@ class traj_analysis:
         self.heatmap.add_representation('cartoon', selection='all', color='grey', opacity=1)
         resid_iter = 0
         if BB_only == True:
-            for resid in df_data.resids:
-                self.heatmap.add_representation(res_rep, selection='backbone and '+str(resid), color=sequential_colors[conts_per_resid_std[resid_iter]], opacity=res_op)
+            for resid in self.analyte_resids:
+                self.heatmap.add_representation(res_rep,selection='backbone and '+str(resid),color=sequential_colors[conts_per_resid_std[resid_iter]],opacity=res_op,radius=res_rad)
                 resid_iter+=1
         else:
-            for resid in df_data.resids:
-                self.heatmap.add_representation(res_rep, selection=str(resid), color=sequential_colors[conts_per_resid_std[resid_iter]], opacity=res_op)
+            for resid in self.analyte_resids:
+                self.heatmap.add_representation(res_rep,selection=str(resid),color=sequential_colors[conts_per_resid_std[resid_iter]],opacity=res_op,radius=res_rad)
                 resid_iter+=1
         self.heatmap._view_height = '800px'
+        print('Done. Run \nobj.heatmap \nin jupyter to view. Run \nnv.write_html("index.html",[obj.heatmap]) \nTo save as an html you can open later.')
         return self.heatmap
     def AA_cont_freq(self,df_data,segid_identifier='protein'):
-        self.AA_contacts_dict = {
+        print('Running')
+        AA_contacts_dict = {
             'CYS': 0, 'ASP': 0, 'SER': 0, 'GLN': 0, 
             'LYS': 0, 'ILE': 0, 'PRO': 0, 'THR': 0, 
             'PHE': 0, 'ASN': 0, 'GLY': 0, 'HSD': 0, 
             'LEU': 0, 'ARG': 0, 'TRP': 0, 'ALA': 0, 
             'VAL':0, 'GLU': 0, 'TYR': 0, 'MET': 0}
-        dic_std_out = self.AA_contacts_dict
-        self.AA_het_at_dict = {
-            'CYS': 3, 'ASP': 5, 'SER': 3, 'GLN': 6, 
-            'LYS': 6, 'ILE': 5, 'PRO': 4, 'THR': 4, 
-            'PHE': 8, 'ASN': 5, 'GLY': 1, 'HSD': 7, 
-            'LEU': 5, 'ARG': 8, 'TRP': 11, 'ALA': 2, 
-            'VAL':4, 'GLU': 6, 'TYR': 9, 'MET': 5}
-        for resid in df_data.resids:
+        AA_het_at_dict = AA_contacts_dict.copy()
+        AA_CH_ratio_dict = AA_contacts_dict.copy()
+        AA_prop_dict = {
+            'CYS': 'Special', 'ASP': '-ve', 'SER': 'Polar', 'GLN': 'Polar', 
+            'LYS': '+ve', 'ILE': 'Hydrophobic', 'PRO': 'Special', 'THR': 'Polar', 
+            'PHE': 'Hydrophobic', 'ASN': 'Polar', 'GLY': 'Special', 'HSD': '+ve', 
+            'LEU': 'Hydrophobic', 'ARG': '+ve', 'TRP': 'Hydrophobic', 'ALA': 'Hydrophobic', 
+            'VAL': 'Hydrophobic', 'GLU': '-ve', 'TYR': 'Hydrophobic', 'MET': 'Hydrophobic'}
+        # Standardise df so that the resids are the index.
+        print('Standardising input')
+        if 'resids' in df_data.columns.values:
+            df_data = df_data.set_index('resids')
+        print('Getting hetro atom counts')
+        for AA in AA_het_at_dict.keys():
+            at_list = self.mda_universe.select_atoms('protein and resname '+str(AA)).residues[0].atoms.types
+            for at in at_list:
+                if at != 'H':
+                    AA_het_at_dict[AA]+=1
+                AA_CH_ratio_dict[AA] = ((at_list=='C').sum()+(at_list=='H').sum())/len(at_list)
+        print('Getting contacts')
+        for resid in self.analyte_resids: # <--- VERY SLOW
             resname = (self.mda_universe.select_atoms('resid '+str(resid)).residues.resnames[0]).upper()
-            self.AA_contacts_dict[resname] += df_data.query('resids == @resid').iloc[0,1:].values.sum()
-        dic_out = {}
-        for x,y in self.AA_contacts_dict.items():
-            if y != 0:
-                dic_out[x] = y # <--- Remove entries with 0 contacts. Very likely a residue which is not in the protein.
-        for resname in dic_out.keys():
+            AA_contacts_dict[resname] += df_data.query('index == @resid').values.sum()
+        # Divide the numnber of contacts by the numnber of that residue in the protein so that they're not over represented.
+        print('Standardising contacts by number of residue type')
+        for resname in AA_contacts_dict.keys():
             occourance_count = 0
             for item in self.mda_universe.select_atoms(segid_identifier).residues.resnames:
                 if item.upper() == resname.upper():
                     occourance_count += 1
             if occourance_count == 0:
-                dic_out[resname] = 0
+                AA_contacts_dict[resname] = 0
             else:
-                dic_out[resname] = dic_out[resname] / occourance_count
-                dic_std_out[resname] = dic_out[resname] / self.AA_contacts_dict[resname]
-        self.AA_conts = pd.DataFrame.from_dict(dic_out, orient='index', columns=['contacts'])
-        self.AA_conts_std = pd.DataFrame.from_dict(dic_std_out, orient='index', columns=['contacts'])
-        return self.AA_conts, self.AA_conts_std
+                AA_contacts_dict[resname] = AA_contacts_dict[resname] / occourance_count
+        # Report the number of hetroatoms in each amino acid so that size can be considered.
+        print('Reporting')
+        self.AA_conts = pd.DataFrame.from_dict(AA_contacts_dict, orient='index', columns=['contacts'])
+        self.AA_conts['het_count'] = AA_het_at_dict.values()
+        self.AA_conts['prop'] = AA_prop_dict.values()
+        self.AA_conts['CH_ratio'] = AA_CH_ratio_dict.values()
+        print('BG. Reported.')
+        return self.AA_conts
     def res_cont_freq(self,df_data):
-        resid_tot_conts = np.zeros(len(df_data.resids))
+        print('Standardising input')
+        if 'resids' in df_data.columns.values:
+            df_data = df_data.set_index('resids')
+        resid_tot_conts = np.zeros(len(df_data.index.values))
         resid_iter = 0
-        for resid in df_data.resids:
-            resid_tot_conts[resid_iter] = df_data.query('resids == @resid').iloc[0,1:].values.sum()
+        for resid in self.analyte_resids:
+            resid_tot_conts[resid_iter] = df_data.query('index == @resid').values.sum()
             resid_iter += 1
         self.cont_per_res = pd.DataFrame({'resids':df_data.resids.values,'contacts':resid_tot_conts})
         return self.cont_per_res
